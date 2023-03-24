b2d2656e83df843971709318060845f0de8730ba
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-24 15:12:12 -0400

    make api call, add req header

--- src/app/services/airports.service.ts
+++ src/app/services/airports.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, retry } from 'rxjs/operators';
+
 import { Airport } from '../components/airports/airport.types';
 import { AIRPORTS } from 'src/app/airportsData';
-import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'X-API-Token': '9265ee96af02fe95e132e33a447f8f08',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AirportsService {
-  constructor() {}
+  private apiUrl = 'http://xiapp.tradewindapps.com:9912/airports';
+
+  constructor(private http: HttpClient) {}
 
   getAirports(): Observable<Airport[]> {
-    //turns AIRPORTS into an observable
-    const airports = of(AIRPORTS);
-    return airports;
+    return this.http.get<Airport[]>(this.apiUrl, httpOptions);
   }
 }
