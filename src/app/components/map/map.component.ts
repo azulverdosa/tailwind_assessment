9b04c4bed0438386969b53eb1cbf1b32529873d3
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 18:54:05 -0400

    change import name and arrange funcrion calls

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,11 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
+import * as Leaflet from 'leaflet';
 import { MarkerService } from '../../services/marker.service';
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = L.icon({
+const iconDefault = Leaflet.icon({
   iconRetinaUrl,
   iconUrl,
   shadowUrl,
@@ -15,7 +15,7 @@ const iconDefault = L.icon({
   tooltipAnchor: [16, -28],
   shadowSize: [41, 41],
 });
-L.Marker.prototype.options.icon = iconDefault;
+Leaflet.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
@@ -24,15 +24,15 @@ L.Marker.prototype.options.icon = iconDefault;
 })
 export class MapComponent implements AfterViewInit {
   //TODO: abstract type into a shared types file, and reuse it where needed
-  private map: L.Map | L.LayerGroup<any>;
+  private map: Leaflet.Map | Leaflet.LayerGroup<any>;
 
   private initMap(): void {
-    this.map = L.map('map', {
+    this.map = Leaflet.map('map', {
       center: [57.6282, -93.5795],
       zoom: 4.1,
     });
 
-    const tiles = L.tileLayer(
+    const tiles = Leaflet.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
         maxZoom: 18,
@@ -45,12 +45,21 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
+  private loadFlights() {
+    // set a timer to do this ever X seconds
+    this.markerService.plotFlightMarkers(this.map);
+  }
+
+  private loadLandmarks() {
+    this.markerService.plotAirportMarkers(this.map);
+    this.markerService.plotCapitalMarkers(this.map);
+  }
+
   constructor(private markerService: MarkerService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
-    this.markerService.plotAirportMarkers(this.map);
-    this.markerService.plotCapitalMarkers(this.map);
-    this.markerService.plotFlightMarkers(this.map);
+    this.loadFlights();
+    this.loadLandmarks();
   }
 }
