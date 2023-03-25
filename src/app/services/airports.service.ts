88696b5cb6ffe8cb6bc760a247f4fd7f3c301156
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 15:53:55 -0400

    remove hard coded data

--- src/app/services/airports.service.ts
+++ src/app/services/airports.service.ts
@@ -6,45 +6,7 @@ import { map, catchError, retry } from 'rxjs/operators';
 
 import { Airport } from '../components/airports/airport.types';
 
-const data = [
-  {
-    icao: 'CYYZ',
-    latitude: 43.676667,
-    longitude: -79.630556,
-    name: 'Toronto Pearson International Airport',
-  },
-  {
-    icao: 'CYVR',
-    latitude: 49.194722,
-    longitude: -123.183889,
-    name: 'Vancouver International Airport',
-  },
-  {
-    icao: 'CYYC',
-    latitude: 51.1225,
-    longitude: -114.013333,
-    name: 'Calgary International Airport',
-  },
-  {
-    icao: 'CYOW',
-    latitude: 45.3225,
-    longitude: -75.667222,
-    name: 'Ottawa Macdonald-Cartier International Airport',
-  },
-  {
-    icao: 'CYEG',
-    latitude: 53.31,
-    longitude: -113.579444,
-    name: 'Edmonton International Airport',
-  },
-  {
-    icao: 'CYHZ',
-    latitude: 44.879722,
-    longitude: -63.510278,
-    name: 'Halifax Stanfield International Airport',
-  },
-];
-
+//create header for http 'options' parameter
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
@@ -56,10 +18,13 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class AirportsService {
+  // define api url
   private apiUrl = 'http://xiapp.tradewindapps.com:9912/airports';
 
+  //add HttpClient as provider
   constructor(private http: HttpClient) {}
 
+  //add observable
   getAirports(): Observable<Airport[]> {
     return this.http.get<Airport[]>(this.apiUrl, httpOptions);
   }
