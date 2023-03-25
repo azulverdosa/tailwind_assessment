399da67cb2dee11f4cfee04676fb6866675c20b4
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 16:31:57 -0400

    import environments

--- src/app/services/airports.service.ts
+++ src/app/services/airports.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, retry } from 'rxjs/operators';
 
 import { Airport } from '../components/airports/airport.types';
+import { environment } from 'src/environments/environment.development';
 
 //create header for http 'options' parameter
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'X-API-Token': '9265ee96af02fe95e132e33a447f8f08',
+    'X-API-Token': environment.API_TOKEN,
   }),
 };
 
@@ -19,7 +20,7 @@ const httpOptions = {
 })
 export class AirportsService {
   // define api url
-  private apiUrl = 'http://xiapp.tradewindapps.com:9912/airports';
+  private apiUrl = `${environment.API_URL}/airports`;
 
   //add HttpClient as provider
   constructor(private http: HttpClient) {}
--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, retry } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.development';
 
 import { Flight } from '../components/flights/flight.types';
 
@@ -10,7 +11,7 @@ import { Flight } from '../components/flights/flight.types';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'X-API-Token': '9265ee96af02fe95e132e33a447f8f08',
+    'X-API-Token': environment.API_TOKEN,
   }),
 };
 
@@ -19,7 +20,7 @@ const httpOptions = {
 })
 export class FlightsService {
   // define api url
-  private apiUrl = 'http://xiapp.tradewindapps.com:9912/currentflights';
+  private apiUrl = `${environment.API_URL}/currentflights`;
 
   //add HttpClient as provider
   constructor(private http: HttpClient) {}
