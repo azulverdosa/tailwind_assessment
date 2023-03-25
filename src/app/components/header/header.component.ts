fb624783bec6d9496e5f5600a61193bb70b31f2f
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 18:52:55 -0400

    clean up code, add icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  faLocationDot,
+  faCircle,
+  faDotCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   title: string = 'Tailwind';
-
-  constructor() {}
-  ngOnInit(): void {}
+  pin = faLocationDot;
+  circle = faCircle;
+  dotCircle = faDotCircle;
 
   updateTimerSelect() {
-    console.log('Time for automatic update');
+    console.log('Refresh for automatic update');
   }
 }
