917d50ce1b8cbeb5c2264aa85d34a8ee6ab53e01
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 15:52:53 -0400

    create capital city database

---
+++ src/app/mock_data/capital_cities.ts
@@ -0,0 +1,80 @@
+export const CAPITALS = [
+  {
+    name: 'Victoria',
+    province: 'Britich Columbia',
+    latitude: 48.4284,
+    longitude: -123.3656,
+  },
+  {
+    name: 'Whitehorse',
+    province: 'Yukon',
+    latitude: 60.7197,
+    longitude: -135.0523,
+  },
+  {
+    name: 'Edmonton',
+    province: 'Alberta',
+    latitude: 53.5461,
+    longitude: -113.4937,
+  },
+  {
+    name: 'Yellowknife',
+    province: 'North West Territories',
+    latitude: 62.454,
+    longitude: -114.3718,
+  },
+  {
+    name: 'Regina',
+    province: 'Saskatchewan',
+    latitude: 50.4452,
+    longitude: -104.6189,
+  },
+  {
+    name: 'Winnipeg',
+    province: 'Manitoba',
+    latitude: 49.8954,
+    longitude: -97.1385,
+  },
+  {
+    name: 'Iqualuit',
+    province: 'Nunavut',
+    latitude: 63.7467,
+    longitude: -68.517,
+  },
+  {
+    name: 'Toronto',
+    province: 'Ontario',
+    latitude: 43.65322,
+    longitude: -79.383,
+  },
+  {
+    name: 'Quebec',
+    province: 'Quebec',
+    latitude: 46.8131,
+    longitude: -71.2075,
+  },
+  {
+    name: 'Fredericton',
+    province: 'New Brunswick',
+    latitude: 45.9636,
+    longitude: -66.6431,
+  },
+  {
+    name: 'Halifax',
+    province: 'Nova Scotia',
+    latitude: 44.8857,
+    longitude: -63.1005,
+  },
+  {
+    name: "St John's",
+    province: 'Newfoundland and Labrador',
+    latitude: 47.5556,
+    longitude: -52.7453,
+  },
+  {
+    name: 'Ottawa',
+    province: 'Canada',
+    latitude: 45.4215,
+    longitude: -75.6962,
+  },
+];
