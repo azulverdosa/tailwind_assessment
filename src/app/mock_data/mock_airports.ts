9244e069155548ebeddbff014c44a06aa2682573
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 13:41:19 -0400

    mock data for testing

---
+++ src/app/mock_data/mock_airports.ts
@@ -0,0 +1,38 @@
+const AIRPORTS = [
+  {
+    icao: 'CYYZ',
+    latitude: 43.676667,
+    longitude: -79.630556,
+    name: 'Toronto Pearson International Airport',
+  },
+  {
+    icao: 'CYVR',
+    latitude: 49.194722,
+    longitude: -123.183889,
+    name: 'Vancouver International Airport',
+  },
+  {
+    icao: 'CYYC',
+    latitude: 51.1225,
+    longitude: -114.013333,
+    name: 'Calgary International Airport',
+  },
+  {
+    icao: 'CYOW',
+    latitude: 45.3225,
+    longitude: -75.667222,
+    name: 'Ottawa Macdonald-Cartier International Airport',
+  },
+  {
+    icao: 'CYEG',
+    latitude: 53.31,
+    longitude: -113.579444,
+    name: 'Edmonton International Airport',
+  },
+  {
+    icao: 'CYHZ',
+    latitude: 44.879722,
+    longitude: -63.510278,
+    name: 'Halifax Stanfield International Airport',
+  },
+];
