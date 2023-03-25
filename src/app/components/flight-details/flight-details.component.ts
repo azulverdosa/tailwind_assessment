bf2754c4658e601cbb2c4f6d0208a796d8c6a986
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-25 13:40:52 -0400

    create and build flights & flight-details components

---
+++ src/app/components/flight-details/flight-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Flight } from '../flights/flight.types';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-flight-details',
+  templateUrl: './flight-details.component.html',
+  styleUrls: ['./flight-details.component.css'],
+})
+export class FlightDetailsComponent implements OnInit {
+  //declare flight input to access each individual flight
+  @Input() flight: Flight;
+  //set icon as a prop
+  faCoffee = faCoffee;
+
+  constructor() {}
+  ngOnInit(): void {}
+}
---
+++ src/app/components/flights/flights.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Flight } from './flight.types';
+import { FlightsService } from 'src/app/services/flights.service';
+
+@Component({
+  selector: 'app-flights',
+  templateUrl: './flights.component.html',
+  styleUrls: ['./flights.component.css'],
+})
+export class FlightsComponent implements OnInit {
+  //assign as property of component
+  flights: Flight[] = [];
+
+  //add FlightService as provider
+  constructor(private flightService: FlightsService) {}
+  ngOnInit(): void {
+    //subscribe to Observable
+    this.flightService.getFlights().subscribe((flights) => {
+      this.flights = flights;
+    });
+  }
+}
