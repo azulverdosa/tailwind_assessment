c2f0b809c22090f79e0e60f35ca08c0cd691aecd
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-04-26 19:18:19 -0400

    add custom marker, create marker Record, save mark records to specific map pmarker

--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -6,14 +6,16 @@ import { FlightsService } from 'src/app/services/flights.service';
 import { Airport } from '../components/airports/airport.types';
 import { AirportsService } from './airports.service';
 
+// import { faPlane } from '@fortawesome/free-solid-svg-icons';
+
 import * as Leaflet from 'leaflet';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarkerService {
-  flights: Flight[] = [];
-  airports: Airport[] = [];
+  flightMarkers: Record<string, Leaflet.Marker> = {};
+  airportMarkers: Record<string, Leaflet.Marker> = {};
 
   constructor(
     private flightService: FlightsService,
@@ -21,20 +23,27 @@ export class MarkerService {
   ) {}
 
   plotFlightMarkers(map: Leaflet.Map | Leaflet.LayerGroup<any>): void {
+    const planeMarker = Leaflet.icon({
+      iconUrl:
+        'https://icones.pro/wp-content/uploads/2021/08/symbole-d-avion-et-de-voyage-vert.png',
+      iconSize: [31, 26], // size of the icon
+      iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
+      popupAnchor: [0, -45], // point from which the popup should open relative to the iconAnchor
+    });
+
     this.flightService.getFlights().subscribe((flights) => {
-      this.flights = flights;
       for (const flight of flights) {
         if (flight.longitude && flight.latitude) {
           const long = flight.longitude;
           const lat = flight.latitude;
 
-          const circle = Leaflet.circleMarker([lat, long], {
-            radius: 3,
-          }).setStyle({
-            color: 'green',
+          const marker = Leaflet.marker([lat, long], {
+            icon: planeMarker,
           });
 
-          circle.addTo(map);
+          marker.addTo(map).bindPopup(flight.flightNumber);
+
+          this.flightMarkers[flight.flightNumber] = marker;
         } else {
           // TODO: I understand that the longitude and latitude information could be null for different reasons, but I'm not sure how to handle the flight when this happens. I would need more information - emailed to ask
         }
@@ -46,7 +55,6 @@ export class MarkerService {
 
   plotAirportMarkers(map: Leaflet.Map | Leaflet.LayerGroup<any>): void {
     this.airportService.getAirports().subscribe((airports) => {
-      this.airports = airports;
       for (const airport of airports) {
         {
           const long = airport.longitude;
@@ -54,7 +62,9 @@ export class MarkerService {
 
           const marker = Leaflet.marker([lat, long]);
 
-          marker.addTo(map);
+          marker.addTo(map).bindPopup(airport.name);
+
+          this.airportMarkers[airport.icao] = marker;
         }
       }
       return airports;
@@ -67,9 +77,10 @@ export class MarkerService {
         const long = cap.longitude;
         const lat = cap.latitude;
 
-        const circle = Leaflet.circleMarker([lat, long], { radius: 0 });
+        // const capitalPopup = Leaflet.popup().setContent(cap.name);
+        const circle = Leaflet.circleMarker([lat, long], { radius: 3 });
 
-        circle.addTo(map);
+        circle.addTo(map).bindPopup(cap.name);
       }
     } catch (err) {
       console.error(err);
