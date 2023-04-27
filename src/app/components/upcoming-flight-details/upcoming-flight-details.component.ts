5ccf0d0cdb45b300707908082a927ca88be13e49
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-04-26 20:34:11 -0400

    add onClicks and import airports

--- src/app/components/upcoming-flight-details/upcoming-flight-details.component.ts
+++ src/app/components/upcoming-flight-details/upcoming-flight-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, AfterViewInit } from '@angular/core';
 
 import { Flight } from '../flights/flight.types';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { Airport } from '../airports/airport.types';
+import { MarkerService } from 'src/app/services/marker.service';
 
 @Component({
   selector: 'app-upcoming-flight-details',
@@ -10,7 +11,17 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class UpcomingFlightDetailsComponent {
   @Input() flight: Flight;
-  faCoffee = faCoffee;
+  @Input() airport: Airport;
+
+  constructor(private markerService: MarkerService) {}
+
+  onClickFlightDetails() {
+    this.markerService.flightMarkers[this.flight.flightNumber]?.openPopup();
+  }
+
+  onClickAirportDetails(icao: string) {
+    this.markerService.airportMarkers[icao]?.openPopup();
+  }
 
   ngAfterViewInit(): void {}
 }
